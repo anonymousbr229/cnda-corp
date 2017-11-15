017728cfafb4b6a7a712a6cad0407b853da984f1
anonymousbr229 <33692952+anonymousbr229@users.noreply.github.com>
2017-11-15 12:17:13 -0200

    Add files via upload

---
+++ cnda.sh
@@ -0,0 +1,30 @@
+#!/files/home
+#script official da CNDA
+
+ c="compania"
+ n="nacional"
+ d="dos"
+ a="anonymous"
+ echo $c $n $d $a
+
+cd /sdcard/Download
+
+touch cnda.txt
+
+
+echo parabéns vc agora é um participante da cnda , seu nome de usuario agora é anonymous >> cnda.txt
+
+
+
+echo seu novo email anonymouscnda@gmail.com >> cnda.txt
+
+
+
+echo sua password cndausr6789 >> cnda.txt
+
+
+
+
+echo seu comprovante de membro esta na sua pasta Downloads na sua memoria interna la tem o seu novo e-mail e senha da conta
+
+touch img.png
